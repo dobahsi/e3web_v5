f02fba337171568f697a30d770f73e32bd3abf4e
dobahsi <hsi@caece.net>
2023-09-10 17:58:23 +0800

    0

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,4 @@
-const time = document.querySelector('.time>span');
+const time = document.querySelector('.time');
 function timeRender() {
     var timeNow = new Date();
     utc8Hours = timeNow.getUTCHours() + 8;
@@ -16,6 +16,21 @@ function timeRender() {
     utc8Hours = utc8Hours < 10 ? '0' + utc8Hours : utc8Hours;
 
     time.innerHTML = `${ampm} ${utc8Hours}:${utc8Minutes}:${utc8Seconds}`;
+
+    // time[0].innerHTML = ampm;
+    // time[1].innerHTML = utc8Hours;
+    // time[2].innerHTML = utc8Minutes;
+    // time[3].innerHTML = utc8Seconds;
+
     setTimeout(timeRender, 1000);
 }
 timeRender();
+
+const tickerBundle = document.querySelector('.ticker-bundle');
+const tickerItem = document.querySelectorAll('.ticker-item');
+const tickerItemCopy = document.querySelectorAll('.ticker-item-copy');
+const tickerMargin = window.getComputedStyle(tickerItem[0]).marginRight;
+const tickerItemsWidth = tickerItem[0].offsetWidth + tickerItem[1].offsetWidth + parseInt(tickerMargin) * 2;
+
+const tickerMovingPerSecond = 50;
+tickerBundle.style.animationDuration = `${tickerItemsWidth / tickerMovingPerSecond}s`;
